4905d7a65de61e85f88ab65e3bd9eaac78821488
Carlos <carlosheadst@gmail.com>
2022-09-17 01:25:02 -0300

    Add project files.

---
+++ SQS.Consumidor/Program.cs
@@ -0,0 +1,19 @@
+﻿using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+var client = new AmazonSQSClient(RegionEndpoint.SAEast1);
+var queueUrl = "https://sqs.sa-east-1.amazonaws.com/283546918190/fila-teste";
+var request = new ReceiveMessageRequest()
+{
+    QueueUrl = queueUrl
+};
+while (true)
+{
+    var response = await client.ReceiveMessageAsync(request);
+    foreach (var message in response.Messages)
+    {
+        Console.WriteLine(message.Body);
+        await client.DeleteMessageAsync(queueUrl, message.ReceiptHandle);
+    }
+}
---
+++ SQS.Fornecedor/Program.cs
@@ -0,0 +1,11 @@
+﻿using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+var client = new AmazonSQSClient(RegionEndpoint.SAEast1);
+var request = new SendMessageRequest()
+{
+    QueueUrl = "https://sqs.sa-east-1.amazonaws.com/283546918190/fila-teste",
+    MessageBody = "Mensagem"
+};
+await client.SendMessageAsync(request);
